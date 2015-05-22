f7331f7cd727c7c52fb65a20a42c3d73c806e96c
unknown <liujianwei@PC201411181101.vcredit.cn>
2015-05-22 17:39:51 +0800

    add script that install chromedriver and selenium-standalone-jar

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Shawn Liu on 2015/5/22.
+ */
+
+var Process = require("child_process");
+
+exports.registerHub = function (port) {
+    
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+var chrome = require("chromedriver");
+console.log(chrome)
+
+var standalone = require("selenium-server-standalone-jar");
+console.log(standalone)
